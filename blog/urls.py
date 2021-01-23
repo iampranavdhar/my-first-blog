38cb00f6d15bdd22fbedfdd89e296be2a365b964
iampranavdhar <nspr2002@gmail.com>
2021-01-24 01:39:29 +0530

    Completed the Djnagogirls Tutorial

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>',views.post_detail,name='post_detail')
+    path('post/<int:pk>',views.post_detail,name='post_detail'), #That <int:pk> is ntng but django is expectecting a integer and returning it as the variable pk
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 from .models import Post
 
 def post_list(request):
@@ -10,3 +12,30 @@ def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
