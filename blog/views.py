dd594344eb83d2eac0fc8d1e25b99c10713626f7
iampranavdhar <nspr2002@gmail.com>
2021-01-24 00:00:18 +0530

    Completed till Extending the application

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
